c19c343fa8fa82f430ef1b95ca51c79c3be817c0
alex <alexliue@outlook.com>
2025-11-04 21:18:29 +0800

    stock_trade_date

--- akshare_sync/data_syn_parallel.py
+++ akshare_sync/data_syn_parallel.py
@@ -12,6 +12,7 @@ from akshare_sync.stock_szse_sector_summary import stock_szse_sector_summary
 from akshare_sync.stock_szse_summary import stock_szse_summary
 from akshare_sync.stock_sse_summary import stock_sse_summary
 from akshare_sync.stock_table_api_summary import stock_table_api_summary
+from akshare_sync.stock_trade_date import stock_trade_date
 from akshare_sync.stock_zh_a_hist_daily_hfq import stock_zh_a_hist_daily_hfq
 from akshare_sync.stock_zh_a_hist_monthly_hfq import stock_zh_a_hist_monthly_hfq
 from akshare_sync.stock_zh_a_hist_weekly_hfq import stock_zh_a_hist_weekly_hfq
@@ -44,9 +45,8 @@ class ProcessPool:
 def sync(max_processes):
     """ 同步的函数列表 """
     functions = [
+        stock_trade_date.sync, #交易日历
         stock_table_api_summary.sync,  # 表 API 接口信息
-        stock_table_api_summary.sync,  # 表 API 接口信息
-        stock_basic_info.sync,  # 股票基本信息: 股票代码、股票名称、交易所、板块
         stock_basic_info.sync,  # 股票基本信息: 股票代码、股票名称、交易所、板块
         stock_short_sale_hk.sync,  # 港股 HK 淡仓申报
         stock_sse_summary.sync,  # 上海证券交易所-股票数据总貌
