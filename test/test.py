e7e83b5af86a426a57c76558b8f28dd54fb8c5e0
alex <494827377@qq.com>
2025-11-06 13:44:12 +0800

    添加振幅字段长度

--- test/test.py
+++ test/test.py
@@ -1,6 +1,10 @@
 
-import tushare as ts
-pro = ts.pro_api()
 
-df = pro.margin_detail(trade_date='20180802')
+from akshare_sync.akshare_overwrite.overwrite_function import stock_zh_a_hist
+
+
+df = stock_zh_a_hist(symbol="600566", period="monthly", start_date=20020131, end_date=20251031, adjust="qfq",
+                     timeout=20)
+
+print(df)
 
